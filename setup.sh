21afc7d160df8b437630fe40a3f779afce9b07d5
JR Morgan <liveaverage@users.noreply.github.com>
2024-12-17 14:27:18 -0500

    Improve handling for non-home ssh dir

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+sudo apt install python3 python3-pip -y
 sudo pip install ansible
-ansible-playbook -c local -i 'localhost,' playbook.yml
+ansible-playbook -c local -i 'localhost,' -b playbook.yml
